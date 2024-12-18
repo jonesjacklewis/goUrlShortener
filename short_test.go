2b1876374afa2ecd195f80b9d9a8e051a5ee5993
jonesjacklewis <jones.jack.lewis@gmail.com>
2024-12-18 23:48:53 +0000

    Consistent hashing

--- short_test.go
+++ short_test.go
@@ -15,4 +15,22 @@ func TestShort(t *testing.T) {
 
 	})
 
+	t.Run("Same url should produce same hash", func(t *testing.T) {
+		url_str := "https://www.google.com"
+		shortened := Short(url_str)
+
+		turns := 100
+
+		for turns > 0 {
+			turns -= 1
+
+			round_hash := Short(url_str)
+
+			if round_hash != shortened {
+				t.Errorf("Short() = %s; want %s", round_hash, shortened)
+			}
+		}
+
+	})
+
 }
